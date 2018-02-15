422158399a50f985d858ce4ffcd30902960b31ff
ginanjar azie <xstepnort@gmail.com>
2018-02-15 23:46:02 +0700

    add table transaksi and detail_transaksi, fixed url

---
+++ transaksisql.sql
@@ -0,0 +1,89 @@
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: tokosandal
+-- ------------------------------------------------------
+-- Server version	5.7.21-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+DROP TABLE IF EXISTS `detail_transaksi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `detail_transaksi` (
+  `ID` int(11) NOT NULL,
+  `ID_TRANSAKSI` int(11) NOT NULL,
+  `ID_PRODUK` int(11) NOT NULL,
+  `HARGA` int(8) NOT NULL,
+  `JUMLAH` int(3) NOT NULL,
+  `SUBTOTAL` int(11) NOT NULL,
+  `TYPE_PRODUK` int(1) NOT NULL,
+  `DETAIL_CUSTOM` varchar(255) NOT NULL,
+  `GAMBAR1` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+LOCK TABLES `detail_transaksi` WRITE;
+/*!40000 ALTER TABLE `detail_transaksi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detail_transaksi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaksi`
+--
+
+DROP TABLE IF EXISTS `transaksi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaksi` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ID_CUSTOMER` int(10) unsigned NOT NULL,
+  `TGL_TRANSAKSI` datetime NOT NULL,
+  `NAMA_PENERIMA` varchar(45) NOT NULL,
+  `ALAMAT_PENERIMA` varchar(225) NOT NULL,
+  `PROVINSI_PENERIMA` varchar(20) NOT NULL,
+  `KOTA_PENERIMA` varchar(15) NOT NULL,
+  `KODEPOS_PENERIMA` varchar(6) NOT NULL,
+  `ONGKIR` int(11) NOT NULL,
+  `STATUS_TRANSAKSI` varchar(25) NOT NULL,
+  `FLAG_DEL` int(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+LOCK TABLES `transaksi` WRITE;
+/*!40000 ALTER TABLE `transaksi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaksi` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-15 23:43:38
