a958aef36992dee51c7c4eea99d1b153bed02fa1
mr-troy <trifathir@gmail.com>
2018-03-28 21:00:17 +0700

    update halaman upload

--- application/views/user/upload-pembayaran.php
+++ application/views/user/upload-pembayaran.php
@@ -17,7 +17,7 @@
 								<span></span>
 							</div>
 						    <input id="buktipembayaran" name="buktipembayaran" type="file" style="color:#ffffff;"></br>	 
-							<input onclick="send();" type="submit" value="SEND">
+							<input id="doSubmit" type="submit" value="SEND">
 						</form>
 					</div>
 				</div>
@@ -27,25 +27,47 @@
 	</div>
  <!--//contact-->
 <script>
- function send(){
-	var dataVal = $('#formUpload').serialize();
+window.onload = function(){
+$("form#formUpload").submit(function(event){
+	
+	var nama = $('#name').val();
+	var pesan = $('#pesan').val();
+	var file = $('#buktipembayaran').val();
+
+	if(nama == ''){
+		bootbox.alert("gagal nama tidak boleh kosong!");
+		return false;
+	}
+	else if(pesan == ''){
+		bootbox.alert("gagal pesan tidak boleh kosong!");
+		return false;
+	}
+	else if(file == ''){
+		bootbox.alert("gagal file tidak boleh kosong!");
+		return false;
+	}
+	event.preventDefault();
+	var dataVal = new FormData($(this)[0]);
+	//alert('aa');
+	//var dataVal=$('#tambahProduk').serialize();
 	$.ajax({
 		url :'<?php echo base_url();?>index.php/user/welcome/sendUploadPembayaran', 
 		type: "post", //form method
 		data: dataVal,
+		contentType: false,
+		processData: false,
 		dataType:"json", //misal kita ingin format datanya brupa json
-		beforeSend:function(){
-		},
 		success:function(result){
 			console.log(result);
 			if(result.code==1){
 				$('#formUpload')[0].reset();
 				bootbox.alert(result.massage);
-				// window.location = "<?php echo base_url();?>admin/produk/index";
 			}else{
 			   bootbox.alert(result.massage);
 			}
 		}
 	});
+});
 }
+
 </script>
